da34e78dd4254415e7c26a41c491da43ef5bb439
Justin Spradlin <justin@blackbayousoftware.co>
2017-08-27 12:08:26 -0500

    Break the build to trigger broken build.

--- api/Controllers/Range.cs
+++ api/Controllers/Range.cs
@@ -12,6 +12,7 @@ namespace api.Controllers
 
         public IEnumerable<TItem> Of<TItem>(Func<TItem> generateItem)
             => Count.Times(i => generateItem());
-                //.OrderBy(n => Sort ? n : default(TItem));
+                // This should trigger a broken build
+                //.OrderBy(n => Sort ? n : default(TItem)); 
     }
 }
